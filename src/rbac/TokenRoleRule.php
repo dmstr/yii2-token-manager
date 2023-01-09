8138285de8b66dbe6ff55f56b6e0c5a930d16526
Elias Luhr <e.luhr@herzogkommunikation.de>
2023-01-09 11:25:46 +0100

    configurable tokenmanager component name in rbac token role rule

--- src/rbac/TokenRoleRule.php
+++ src/rbac/TokenRoleRule.php
@@ -2,19 +2,28 @@
 
 namespace dmstr\tokenManager\rbac;
 
+use dmstr\tokenManager\components\TokenManager;
 use dmstr\tokenManager\exceptions\LoadTokenException;
+use yii\base\InvalidConfigException;
 use yii\rbac\Rule;
 use Yii;
 
 class TokenRoleRule extends Rule
 {
+
+    public string $tokenManager = 'tokenManager';
+
     /**
      * @inheritdoc
+     *
+     * @throws InvalidConfigException
      */
     public function execute($user, $item, $params)
     {
         try {
-            $roles = \Yii::$app->tokenManager->getRoles();
+            /** @var TokenManager $tokenManager */
+            $tokenManager = Yii::$app->get($this->tokenManager);
+            $roles = $tokenManager->getRoles();
         } catch (LoadTokenException $exception) {
             Yii::error($exception->getMessage());
             return false;
